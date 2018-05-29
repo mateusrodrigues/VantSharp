544ad97c035140334b73730ee5ed328154849790
mateusrodrigues <mateus.rodrigues@live.com>
2018-05-29 14:41:00 -0300

    Enable logging output for encode verb

--- Configuration/CommandLineParserOptions.cs
+++ Configuration/CommandLineParserOptions.cs
@@ -3,10 +3,17 @@ using CommandLine;
 
 namespace VantSharp.Configuration
 {
-    [Verb("encode", HelpText = "Encodes a file for hexadecimal transmission")]
+    [Verb("encode", HelpText = "Encodes a file for hexadecimal transmission.")]
     public class EncodeOptions
     {
-        [Option('f', "file", Required = true, HelpText = "File to be encoded for transmission")]
+        [Option('f', "file", Required = true, HelpText = "File to be encoded for transmission.")]
         public string InputFile { get; set; }
+
+        [Option('v', "verbose", Required = false, HelpText = "Turn on verbose output.")]
+        public bool Verbose { get; set; }
+
+        [Option('l', "log", Required = false, HelpText = "Write logging output to a file."
+            + "This file has a .log extension by design.")]
+        public string LogOutput { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using CommandLine;
+using Serilog;
 using VantSharp.Configuration;
 using VantSharp.Models;
 
@@ -20,7 +21,19 @@ namespace VantSharp
 
         private static int RunEncodeAndReturnExitCode(EncodeOptions opts)
         {
-            Console.WriteLine($"Reading from: {opts.InputFile}");
+            // Configure Logger
+            var loggerConfiguration = new LoggerConfiguration();
+            loggerConfiguration.MinimumLevel.Debug();
+            // If verbose flag was passed in, write output to console
+            if (opts.Verbose)
+                loggerConfiguration.WriteTo.Console();
+            // If a log output file was passed in, write to that file
+            if (!string.IsNullOrEmpty(opts.LogOutput)
+                || !string.IsNullOrWhiteSpace(opts.LogOutput))
+                loggerConfiguration.WriteTo.File($"{opts.LogOutput}.log");
+            Log.Logger = loggerConfiguration.CreateLogger();
+
+            Log.Information($"Reading from: {opts.InputFile}");
 
             using (Stream source = File.OpenRead(opts.InputFile))
             {
@@ -32,9 +45,14 @@ namespace VantSharp
 
                 while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    // Copy elements from buffer to new content array
-                    // by 'bytesRead' amount to avoid junk content
-                    // from previous iteration runs.
+                    // Copy elements from buffer to new content array by
+                    // 'bytesRead' amount to avoid junk content from previous
+                    // iteration runs. This happens when previously allocated
+                    // byte arrays of size 'x' are reused to store data of sizes
+                    // smaller than 'x', so, the amount unused are kept with
+                    // their original data which I refer to as "junk".
+                    // Therefore, the solution used is to reallocate the byte
+                    // array with the exact size it needs to have.
                     byte[] content = new byte[bytesRead];
                     Array.ConstrainedCopy(buffer, 0, content, 0, bytesRead);
                     // Create a new Packet object to store information gathered
@@ -47,7 +65,7 @@ namespace VantSharp
                     transmission.Packets.Add(packet);
                 }
 
-                Console.WriteLine($"Your transmission has {transmission.PacketCount} packets");
+                Log.Information($"Your transmission has {transmission.PacketCount} packets");
             }
 
             return 0;
